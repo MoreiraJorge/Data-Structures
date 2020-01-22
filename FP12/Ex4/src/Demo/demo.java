e51725268a1b9fb97295fd32578120f12f546edd
8160297 <8160297@estg.ipp.pt>
2020-01-22 19:13:13 +0000

    missing shortest path

--- FP12/Ex4/src/Demo/demo.java
+++ FP12/Ex4/src/Demo/demo.java
@@ -1,45 +1,49 @@
 package Demo;
 
-import Graph.Graph;
-import Graph.GraphExceptions;
+import Graph.GraphInMatrix;
 
 import java.util.Iterator;
 
+import Graph.GraphExceptions;
+import Network.NetworkADT;
+import Network.NetworkMatrix;
+import Stack.EmptyCollectionException;
+
 public class demo {
 
-    public static void tests(int test) throws GraphExceptions {
-        Graph<Integer> graph;
+    public static void tests(int test) throws GraphExceptions, EmptyCollectionException {
+        NetworkADT<Integer> graph;
         Iterator<Integer> itr;
 
         switch (test) {
             // Add Test
             case 1:
-                graph = new Graph();
+                graph = new NetworkMatrix();
                 graph.addVertex(1);
                 graph.addVertex(2);
                 graph.addVertex(3);
                 graph.addVertex(4);
-                graph.addEdge(1, 2);
-                graph.addEdge(2, 4);
-                graph.addEdge(2, 2);
-                graph.addEdge(1, 1);
-                graph.addEdge(4, 4);
-                graph.addEdge(1, 4);
+                graph.addEdge(1, 2, 3);
+                graph.addEdge(2, 4, 4);
+                graph.addEdge(2, 2, 5);
+                graph.addEdge(1, 1, 6);
+                graph.addEdge(4, 4, 7);
+                graph.addEdge(1, 4, 8);
                 System.out.println(graph.toString());
                 break;
             //ShortestPath Not Found Test
             case 2:
-                graph = new Graph();
+                graph = new NetworkMatrix();
                 graph.addVertex(1);
                 graph.addVertex(2);
                 graph.addVertex(3);
                 graph.addVertex(4);
-                graph.addEdge(1, 2);
-                graph.addEdge(2, 4);
-                graph.addEdge(2, 2);
-                graph.addEdge(1, 1);
-                graph.addEdge(4, 4);
-                graph.addEdge(1, 4);
+                graph.addEdge(1, 2, 3);
+                graph.addEdge(2, 4, 4);
+                graph.addEdge(2, 2, 5);
+                graph.addEdge(1, 1, 6);
+                graph.addEdge(4, 4, 7);
+                graph.addEdge(1, 4, 8);
                 Iterator itrSP = graph.iteratorShortestPath(1, 3);
                 while (itrSP.hasNext()) {
                     System.out.print("->");
@@ -49,17 +53,17 @@ public class demo {
                 break;
             //ShortestPath Found Test
             case 3:
-                graph = new Graph();
+                graph = new NetworkMatrix();
                 graph.addVertex(1);
                 graph.addVertex(2);
                 graph.addVertex(3);
                 graph.addVertex(4);
-                graph.addEdge(1, 2);
-                graph.addEdge(2, 4);
-                graph.addEdge(2, 2);
-                graph.addEdge(1, 1);
-                graph.addEdge(4, 4);
-                graph.addEdge(1, 4);
+                graph.addEdge(1, 2, 3);
+                graph.addEdge(2, 4, 4);
+                graph.addEdge(2, 2, 5);
+                graph.addEdge(1, 1, 6);
+                graph.addEdge(4, 4, 7);
+                graph.addEdge(1, 4, 8);
                 Iterator itrSPFound = graph.iteratorShortestPath(1, 4);
                 while (itrSPFound.hasNext()) {
                     System.out.print("->");
@@ -69,33 +73,35 @@ public class demo {
                 break;
             // Remove Test
             case 4:
-                graph = new Graph();
+                graph = new NetworkMatrix();
                 graph.addVertex(1);
                 graph.addVertex(2);
                 graph.addVertex(3);
                 graph.addVertex(4);
-                graph.addEdge(1, 2);
-                graph.addEdge(2, 4);
-                graph.addEdge(2, 2);
-                graph.addEdge(1, 1);
-                graph.addEdge(4, 4);
-                graph.addEdge(1, 4);
+                graph.addEdge(1, 2, 3);
+                graph.addEdge(2, 4, 4);
+                graph.addEdge(2, 2, 5);
+                graph.addEdge(1, 1, 6);
+                graph.addEdge(4, 4, 7);
+                graph.addEdge(1, 4, 8);
                 System.out.println(graph.toString());
                 graph.removeVertex(3);
                 System.out.println(graph.toString());
-                // BFS Itr Test
+                break;
+            // BFS Itr Test
             case 5:
-                graph = new Graph();
+                graph = new NetworkMatrix();
                 graph.addVertex(1);
                 graph.addVertex(2);
                 graph.addVertex(3);
                 graph.addVertex(4);
-                graph.addEdge(1, 2);
-                graph.addEdge(2, 4);
-                graph.addEdge(2, 2);
-                graph.addEdge(1, 1);
-                graph.addEdge(4, 4);
-                graph.addEdge(1, 4);
+                graph.addEdge(1, 2, 3);
+                graph.addEdge(2, 4, 4);
+                graph.addEdge(2, 2, 5);
+                graph.addEdge(1, 1, 6);
+                graph.addEdge(4, 4, 7);
+                graph.addEdge(1, 3, 3);
+                graph.addEdge(1, 4, 8);
                 System.out.println(graph.toString());
                 itr = graph.iteratorBFS(1);
                 while (itr.hasNext()) {
@@ -104,20 +110,75 @@ public class demo {
                 break;
             // Connected Test
             case 6:
-                graph = new Graph();
+                graph = new NetworkMatrix();
                 graph.addVertex(1);
                 graph.addVertex(2);
                 graph.addVertex(3);
                 graph.addVertex(4);
-                graph.addEdge(1, 2);
-                graph.addEdge(2, 4);
-                graph.addEdge(2, 2);
-                graph.addEdge(1, 1);
-                graph.addEdge(4, 3);
-                graph.addEdge(4, 4);
-                graph.addEdge(1, 4);
+                graph.addEdge(1, 2, 3);
+                graph.addEdge(2, 4, 4);
+                graph.addEdge(2, 2, 5);
+                graph.addEdge(1, 1, 6);
+                graph.addEdge(4, 4, 7);
+                graph.addEdge(1, 4, 8);
+                graph.addEdge(4, 3, 6);
                 System.out.println(graph.toString());
                 System.out.println("\nExcepted: True. Result : " + graph.isConnected());
+                break;
+            case 7:
+                graph = new NetworkMatrix();
+                graph.addVertex(1);
+                graph.addVertex(2);
+                graph.addVertex(3);
+                graph.addVertex(4);
+                graph.addEdge(1, 2, 3);
+                graph.addEdge(2, 4, 4);
+                graph.addEdge(2, 2, 5);
+                graph.addEdge(1, 1, 6);
+                graph.addEdge(4, 4, 7);
+                graph.addEdge(1, 4, 8);
+                graph.addEdge(4, 3, 6);
+                System.out.println(graph.toString());
+                graph.addVertex(5);
+                System.out.println(graph.toString());
+                graph.addEdge(1, 5, 9);
+                System.out.println(graph.toString());
+                break;
+            case 8:
+                graph = new NetworkMatrix();
+                graph.addVertex(1);
+                graph.addVertex(2);
+                graph.addVertex(3);
+                graph.addVertex(4);
+                graph.addEdge(1, 2, 3);
+                graph.addEdge(2, 4, 4);
+                graph.addEdge(2, 2, 5);
+                graph.addEdge(1, 1, 6);
+                graph.addEdge(4, 4, 7);
+                graph.addEdge(1, 4, 8);
+                System.out.println(graph.toString());
+                graph.removeEdge(1, 1);
+                System.out.println(graph.toString());
+                break;
+            case 9:
+                graph = new NetworkMatrix();
+                graph.addVertex(1);
+                graph.addVertex(2);
+                graph.addVertex(3);
+                graph.addVertex(4);
+                graph.addEdge(1, 2, 3);
+                graph.addEdge(2, 4, 4);
+                graph.addEdge(2, 2, 5);
+                graph.addEdge(1, 1, 6);
+                graph.addEdge(4, 4, 7);
+                graph.addEdge(1, 4, 8);
+                graph.addEdge(4, 3, 6);
+                System.out.println(graph.toString());
+                System.out.println(graph.toString());
+                itr = graph.iteratorDFS(1);
+                while (itr.hasNext()) {
+                    System.out.println(itr.next());
+                }
                 break;
             default:
                 break;
@@ -125,11 +186,12 @@ public class demo {
 
     }
 
-    public static void main(String[] args) throws GraphExceptions {
+    public static void main(String[] args) throws GraphExceptions, EmptyCollectionException {
         System.out.println("----------------------------------");
-        for (int i = 1; i <= 6; ++i){
+        for (int i = 1; i <= 9; ++i) {
             tests(i);
             System.out.println("----------------------------------");
         }
     }
 }
+
---
+++ FP12/Ex4/src/Network/NetworkMatrix.java
@@ -0,0 +1,134 @@
+package Network;
+
+import Graph.GraphExceptions;
+import Graph.GraphInMatrix;
+
+/**
+ * @param <T>
+ */
+public class NetworkMatrix<T> extends GraphInMatrix<T> implements NetworkADT<T> {
+
+    protected double[][] weightMatrix;
+
+    public NetworkMatrix() {
+        this.weightMatrix = new double[DEFAULT_CAPACITY][DEFAULT_CAPACITY];
+    }
+
+
+    @Override
+    public void addVertex(T vertex) {
+        if (numVertices == vertices.length) {
+            expandCapacity();
+        }
+
+        vertices[numVertices] = vertex;
+        for (int i = 0; i < adjMatrix.length; i++) {
+            adjMatrix[numVertices][i] = false;
+            adjMatrix[i][numVertices] = false;
+        }
+        numVertices++;
+    }
+
+    /**
+     *
+     */
+    private void expandCapacity() {
+        T[] tmp = (T[]) (new Object[vertices.length + DEFAULT_CAPACITY]);
+        for (int i = 0; i < vertices.length; i++) {
+            tmp[i] = vertices[i];
+        }
+
+        vertices = tmp;
+
+        boolean[][] tmp_mat = new boolean[tmp.length][tmp.length];
+        for (int j = 0; j < adjMatrix.length; j++) {
+            for (int k = 0; k < adjMatrix.length; k++) {
+                tmp_mat[j][k] = adjMatrix[j][k];
+            }
+        }
+
+        adjMatrix = tmp_mat;
+
+        double[][] tmp_Wmat = new double[tmp.length][tmp.length];
+        for (int j = 0; j < weightMatrix.length; j++) {
+            for (int k = 0; k < weightMatrix.length; k++) {
+                tmp_Wmat[j][k] = weightMatrix[j][k];
+            }
+        }
+
+        weightMatrix = tmp_Wmat;
+    }
+
+    @Override
+    public void removeVertex(T vertex) throws GraphExceptions {
+
+        if (isEmpty()) {
+            throw new GraphExceptions(GraphExceptions.EMPTY_GRAPH);
+        }
+
+        int index = getIndex(vertex);
+
+        if (index == -1) {
+            throw new GraphExceptions(GraphExceptions.ELEMENT_NOT_FOUND);
+        }
+
+        for (int i = index; i < numVertices - 1; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                adjMatrix[j][i] = adjMatrix[j][i + 1];
+                weightMatrix[j][i] = weightMatrix[j][i + 1];
+            }
+
+        }
+
+        for (int i = index; i < numVertices - 1; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                adjMatrix[i][j] = adjMatrix[i + 1][j];
+                weightMatrix[i][j] = weightMatrix[i + 1][j];
+            }
+
+        }
+
+        for (int h = index; h < numVertices - 1; h++) {
+            vertices[h] = vertices[h + 1];
+        }
+
+        numVertices--;
+
+    }
+
+    @Override
+    public void addEdge(T vertex1, T vertex2, double weight) throws GraphExceptions {
+        int indexVertex1 = getIndex(vertex1);
+        int indexVertex2 = getIndex(vertex2);
+
+        if (indexIsValid(indexVertex1) && indexIsValid(indexVertex2)) {
+            addEdge(vertex1, vertex2);
+            weightMatrix[indexVertex1][indexVertex2] = weight;
+            weightMatrix[indexVertex2][indexVertex1] = weight;
+        } else {
+            throw new GraphExceptions(GraphExceptions.ELEMENT_NOT_FOUND);
+        }
+    }
+
+    @Override
+    public double shortestPathWeight(T vertex1, T vertex2) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        String text = "";
+
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                text += "|";
+                text += weightMatrix[i][j];
+                text += "|";
+            }
+            text += "\n";
+        }
+
+        return text;
+    }
+
+}
