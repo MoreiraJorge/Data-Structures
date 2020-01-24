2313929a9eff92eaeeacd68698ff47d317c3f222
8160297 <8160297@estg.ipp.pt>
2020-01-24 16:33:20 +0000

    fixed

---
+++ FP12/Ex5/src/Network/NetworkList.java
@@ -0,0 +1,21 @@
+package Network;
+
+import BinaryTree.BinaryTreeExceptions;
+import Graph.GraphExceptions;
+import Graph.GraphInLIst;
+
+public class NetworkList<T> extends GraphInLIst<T> implements NetworkADT<T> {
+
+    @Override
+    public void addEdge(T vertex1, T vertex2, double weight) throws GraphExceptions {
+
+
+    }
+
+    @Override
+    public double shortestPathWeight(T vertex1, T vertex2) throws BinaryTreeExceptions, GraphExceptions {
+
+        return 0;
+    }
+
+}
---
+++ FP12/Ex5/src/Network/NetworkNode.java
@@ -0,0 +1,19 @@
+package Network;
+
+import Graph.GraphNode;
+
+public class NetworkNode<T> extends GraphNode<T> {
+
+    protected double weight;
+
+    /**
+     *
+      * @param element
+     * @param weight
+     */
+    public NetworkNode(T element, double weight) {
+        super(element);
+        this.weight = weight;
+    }
+
+}
